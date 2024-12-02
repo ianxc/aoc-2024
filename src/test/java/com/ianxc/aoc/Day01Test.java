d9541c11264726987264320266fe1a50af76c7dd
Ian Chen <ianchen7777@gmail.com>
2024-12-02 20:07:20 +1100

    Day 02 done (likely non-optimal time complexity)

--- src/test/java/com/ianxc/aoc/Day01Test.java
+++ src/test/java/com/ianxc/aoc/Day01Test.java
@@ -6,14 +6,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day01Test {
     @Test
-    void testDay01() {
+    void testPart1() {
         var result = Day01.part1("day01/input.txt");
 
         assertEquals(1530215, result);
     }
 
     @Test
-    void testDay02() {
+    void testPart2() {
         var result = Day01.part2("day01/input.txt");
 
         assertEquals(26800609, result);
