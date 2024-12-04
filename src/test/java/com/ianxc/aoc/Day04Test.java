92650bf0e04a9d4518b6ecde0ff3650beafd7c2d
Ian Chen <ianchen7777@gmail.com>
2024-12-04 13:15:12 +1100

    Day 04 setup

---
+++ src/test/java/com/ianxc/aoc/Day04Test.java
@@ -0,0 +1,16 @@
+package com.ianxc.aoc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day04Test {
+    @Test
+    void testPart1() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void testPart2() {
+    }
+}
