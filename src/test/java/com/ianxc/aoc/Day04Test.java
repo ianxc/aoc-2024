836beb5e5ba50707c3a460aed07733edcdf5057c
Ian Chen <ianchen7777@gmail.com>
2024-12-04 16:50:13 +1100

    Day 04 done!

--- src/test/java/com/ianxc/aoc/Day04Test.java
+++ src/test/java/com/ianxc/aoc/Day04Test.java
@@ -7,10 +7,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Day04Test {
     @Test
     void testPart1() {
-        assertEquals(1, 1);
+        assertEquals(2358, Day04.part1("day04/input.txt"));
     }
 
     @Test
     void testPart2() {
+        assertEquals(1737, Day04.part2("day04/input.txt"));
     }
 }
