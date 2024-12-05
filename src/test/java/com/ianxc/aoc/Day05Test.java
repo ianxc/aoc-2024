337323b43728b56871800af14e830f8e9763cd8b
Ian Chen <ianchen7777@gmail.com>
2024-12-05 17:10:27 +1100

    Day 05 wip

---
+++ src/test/java/com/ianxc/aoc/Day05Test.java
@@ -0,0 +1,14 @@
+package com.ianxc.aoc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day05Test {
+
+    @Test
+    void testPart1() {
+        Day05.part1("day05/input.txt");
+    }
+
+}
