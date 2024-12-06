0cf0e0140c31db8ba96a87a22085d2f2f853a8b6
Ian Chen <ianchen7777@gmail.com>
2024-12-06 16:28:16 +1100

    Day 06 part 1 done

---
+++ src/test/java/com/ianxc/aoc/Day06Test.java
@@ -0,0 +1,26 @@
+package com.ianxc.aoc;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day06Test {
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    void testPart1() {
+        var res = Day06.part1("day06/input.txt");
+
+        assertEquals(4433, res);
+    }
+
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    void testPart1demo() {
+        var res = Day06.part1("day06/demo.txt");
+
+        assertEquals(41, res);
+    }
+}
