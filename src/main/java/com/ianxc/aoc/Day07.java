1346375ff055741ae52e1571cd0569f712878337
Ian Chen <ianchen7777@gmail.com>
2024-12-07 18:35:29 +1100

    Day 07 part 2 done

--- src/main/java/com/ianxc/aoc/Day07.java
+++ src/main/java/com/ianxc/aoc/Day07.java
@@ -2,18 +2,19 @@ package com.ianxc.aoc;
 
 import java.util.Arrays;
 
+@SuppressWarnings("DuplicatedCode")
 public class Day07 {
     static long part1(String path) {
         return Util.loadFile(path)
-                .stream()
+                .parallelStream()
                 .map(s -> s.split(" "))
                 .map(xs -> {
                     var desiredStr = xs[0].substring(0, xs[0].length() - 1);
                     var components = Arrays.stream(xs).skip(1).mapToLong(Long::parseLong).toArray();
                     return new Spec(Long.parseLong(desiredStr), components);
                 })
-                .peek(spec -> System.out.printf("spec: desired=%d components=%s\n",
-                        spec.desired, Arrays.toString(spec.components)))
+//                .peek(spec -> System.out.printf("spec: desired=%d components=%s\n",
+//                        spec.desired, Arrays.toString(spec.components)))
                 .filter(spec -> dfs(spec, 1, spec.components[0]))
                 .mapToLong(spec -> spec.desired)
                 .sum();
@@ -32,8 +33,37 @@ public class Day07 {
                 dfs(spec, currIndex + 1, currValue + spec.components[currIndex]);
     }
 
+    static boolean dfs2(Spec spec, int currIndex, long currValue) {
+        if (currIndex == spec.components.length) {
+            return spec.desired == currValue;
+        }
+        if (spec.desired < currValue) {
+            return false;
+        }
+
+        return dfs2(spec, currIndex + 1, currValue * spec.components[currIndex]) ||
+                dfs2(spec, currIndex + 1, currValue + spec.components[currIndex]) ||
+                dfs2(spec, currIndex + 1, currValue * nextPow10(spec.components[currIndex]) + spec.components[currIndex]);
+    }
+
+    static long nextPow10(long n) {
+        return (long) Math.pow(10, 1 + Math.floor(Math.log10(n)));
+    }
+
     static long part2(String path) {
-        return 0;
+        return Util.loadFile(path)
+                .parallelStream()
+                .map(s -> s.split(" "))
+                .map(xs -> {
+                    var desiredStr = xs[0].substring(0, xs[0].length() - 1);
+                    var components = Arrays.stream(xs).skip(1).mapToLong(Long::parseLong).toArray();
+                    return new Spec(Long.parseLong(desiredStr), components);
+                })
+//                .peek(spec -> System.out.printf("spec: desired=%d components=%s\n",
+//                        spec.desired, Arrays.toString(spec.components)))
+                .filter(spec -> dfs2(spec, 1, spec.components[0]))
+                .mapToLong(spec -> spec.desired)
+                .sum();
     }
 
     record Spec(long desired, long[] components) {
