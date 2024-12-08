35c950c661f111c9f745a3e7577b3fc0178f2caf
Ian Chen <ianchen7777@gmail.com>
2024-12-08 18:33:59 +1100

    Day 08 part 1 done

--- src/main/java/com/ianxc/aoc/Day08.java
+++ src/main/java/com/ianxc/aoc/Day08.java
@@ -1,4 +1,58 @@
 package com.ianxc.aoc;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Day08 {
+    static int part1(String path) {
+        var grid = Util.loadFile(path).stream().map(String::toCharArray).toArray(char[][]::new);
+        var height = grid.length;
+        var width = grid[0].length;
+
+        var antennae = new HashMap<Character, List<Point>>();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (grid[i][j] != '.') {
+                    antennae.computeIfAbsent(grid[i][j], k -> new ArrayList<>()).add(new Point(i, j));
+                }
+            }
+        }
+
+        return (int) antennae.entrySet()
+                .stream()
+                .flatMap(e ->
+                        pairsOf(e.getValue())
+                                .stream()
+                                .flatMap(pair -> {
+                                            var di = pair.right.i - pair.left.i;
+                                            var dj = pair.right.j - pair.left.j;
+                                            var righter = new Point(pair.right.i + di, pair.right.j + dj);
+                                            var lefter = new Point(pair.left.i - di, pair.left.j - dj);
+                                            return Stream.of(righter, lefter);
+                                        }
+                                )
+                                .filter(point -> point.i >= 0 && point.i < height &&
+                                        point.j >= 0 && point.j < width))
+                .distinct()
+                .count();
+    }
+
+    static <T> List<Pair<T>> pairsOf(List<T> items) {
+        var n = items.size();
+        var result = new ArrayList<Pair<T>>(n / 2 * n);
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                result.add(new Pair<>(items.get(i), items.get(j)));
+            }
+        }
+        return result;
+    }
+
+    record Point(int i, int j) {
+    }
+
+    record Pair<T>(T left, T right) {
+    }
 }
--- src/test/java/com/ianxc/aoc/Day08Test.java
+++ src/test/java/com/ianxc/aoc/Day08Test.java
@@ -1,7 +1,21 @@
 package com.ianxc.aoc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day08Test {
+    @Test
+    void testPart1Demo() {
+        var res = Day08.part1("day08/demo.txt");
 
+        assertEquals(14, res);
+    }
+
+    @Test
+    void testPart1() {
+        var res = Day08.part1("day08/input.txt");
+
+        assertEquals(396, res);
+    }
 }
