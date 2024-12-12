2b4038096052397354b7807ec1b0e3e6686994dd
Ian Chen <ianchen7777@gmail.com>
2024-12-12 21:57:56 +1100

    Day 12 further attempts

--- src/main/java/com/ianxc/aoc/Day12.java
+++ src/main/java/com/ianxc/aoc/Day12.java
@@ -1,6 +1,11 @@
 package com.ianxc.aoc;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class Day12 {
     private static final int[][] offsets = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
@@ -13,7 +18,7 @@ public class Day12 {
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
                 if (!Character.isLowerCase(grid[i][j])) {
-                    final var region = exploreRegion(grid, i, j);
+                    final var region = exploreRegionPerimeter(grid, i, j);
                     // System.out.println(region);
                     totalPrice += region.area * region.perimeter;
                 }
@@ -22,7 +27,11 @@ public class Day12 {
         return totalPrice;
     }
 
-    static ExploredRegion exploreRegion(char[][] grid, int i, int j) {
+    static char[][] parseInput(String path) {
+        return Util.loadFile(path).stream().map(String::toCharArray).toArray(char[][]::new);
+    }
+
+    static ExploredRegion exploreRegionPerimeter(char[][] grid, int i, int j) {
         final var height = grid.length;
         final var width = grid[0].length;
         final var currChar = grid[i][j];
@@ -30,9 +39,11 @@ public class Day12 {
         // System.out.printf("Explore region i=%d j=%d char=%c\n", i, j, currChar);
         var area = 0L;
         var perimeter = 0L;
+
         final var queue = new ArrayDeque<Point>();
         grid[i][j] = visitedChar;
         queue.offer(new Point(i, j));
+
         while (!queue.isEmpty()) {
             final var curr = queue.poll();
             // record statistics and mark as visited
@@ -59,13 +70,88 @@ public class Day12 {
         return new ExploredRegion(currChar, area, perimeter);
     }
 
-    static char[][] parseInput(String path) {
-        return Util.loadFile(path).stream().map(String::toCharArray).toArray(char[][]::new);
+    static long part2(String path) {
+        final var grid = parseInput(path);
+        final var height = grid.length;
+        final var width = grid[0].length;
+        final var visited = new boolean[height][width];
+        var totalPrice = 0L;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (!visited[i][j]) {
+                    final var region = exploreRegionSides(grid, i, j, visited);
+                    System.out.println(region);
+                    totalPrice += region.area * region.perimeter;
+                }
+            }
+        }
+        return totalPrice;
+    }
+
+    static ExploredRegion exploreRegionSides(char[][] grid, int i, int j, boolean[][] visited) {
+        final var height = grid.length;
+        final var width = grid[0].length;
+        final var currChar = grid[i][j];
+        var area = 0L;
+
+        final var stack = new ArrayDeque<Point>();
+        stack.push(new Point(i, j));
+        visited[i][j] = true;
+        // Store horizontally-spanning points (j), indexed by a 2x1 vertical block
+        var fenceV = new HashMap<SingleAxisPair, List<Integer>>();
+        // Store vertically-spanning points (i), indexed by a 1x2 horizontal block
+        var fenceH = new HashMap<SingleAxisPair, List<Integer>>();
+
+        while (!stack.isEmpty()) {
+            final var curr = stack.pop();
+            area++;
+            for (final var offset : offsets) {
+                final var newI = curr.i + offset[0];
+                final var newJ = curr.j + offset[1];
+                if (newI >= 0 && newJ >= 0) {
+                    if (newI < height && newJ < width && grid[newI][newJ] == currChar) {
+                        if (!visited[newI][newJ]) {
+                            visited[newI][newJ] = true;
+                            stack.push(new Point(newI, newJ));
+                        }
+                        continue;
+                    }
+                }
+                if (curr.i != newI) {
+                    // changed i
+                    fenceV.computeIfAbsent(new SingleAxisPair(curr.i, newI), k -> new ArrayList<>())
+                            .add(j);
+                } else {
+                    // changed j
+                    fenceH.computeIfAbsent(new SingleAxisPair(curr.j, newJ), k -> new ArrayList<>())
+                            .add(i);
+                }
+            }
+        }
+
+        var sides = Stream.concat(fenceV.values().stream(), fenceH.values().stream())
+                .mapToLong(spanningPoints -> {
+                    Collections.sort(spanningPoints);
+                    var fence = 1;
+                    for (int idx = 1; idx < spanningPoints.size(); idx++) {
+                        if (spanningPoints.get(idx) - spanningPoints.get(idx - 1) != 1) {
+                            fence += 1;
+                        }
+                    }
+                    return fence;
+                })
+                .sum();
+
+
+        return new ExploredRegion(currChar, area, sides);
     }
 
     record ExploredRegion(char ch, long area, long perimeter) {
     }
 
     record Point(int i, int j) {
+    }
+
+    record SingleAxisPair(int x, int newX) {
     }
 }
