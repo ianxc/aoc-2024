1ab21e10092ca24328836e09b72262fa00765f9e
Ian Chen <ianchen7777@gmail.com>
2024-12-12 19:54:05 +1100

    Day 12 part 1 working

--- src/main/java/com/ianxc/aoc/Day12.java
+++ src/main/java/com/ianxc/aoc/Day12.java
@@ -3,8 +3,6 @@ package com.ianxc.aoc;
 import java.util.ArrayDeque;
 
 public class Day12 {
-    private static final char VISITED_MARKER = '.';
-
     private static final int[][] offsets = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 
     static long part1(String path) {
@@ -14,9 +12,9 @@ public class Day12 {
         var totalPrice = 0L;
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                if (grid[i][j] != VISITED_MARKER) {
+                if (!Character.isLowerCase(grid[i][j])) {
                     final var region = exploreRegion(grid, i, j);
-                    System.out.println(region);
+                    // System.out.println(region);
                     totalPrice += region.area * region.perimeter;
                 }
             }
@@ -28,31 +26,35 @@ public class Day12 {
         final var height = grid.length;
         final var width = grid[0].length;
         final var currChar = grid[i][j];
-        System.out.printf("Explore region i=%d j=%d char=%c\n", i, j, currChar);
+        final var visitedChar = Character.toLowerCase(currChar);
+        // System.out.printf("Explore region i=%d j=%d char=%c\n", i, j, currChar);
         var area = 0L;
         var perimeter = 0L;
         final var queue = new ArrayDeque<Point>();
+        grid[i][j] = visitedChar;
         queue.offer(new Point(i, j));
-        grid[i][j] = VISITED_MARKER;
         while (!queue.isEmpty()) {
             final var curr = queue.poll();
             // record statistics and mark as visited
-            var perimeterContribution = 0L;
+            // Count edges that are external or adjacent to different characters
+            area++;
             for (final var offset : offsets) {
                 final var newI = curr.i + offset[0];
                 final var newJ = curr.j + offset[1];
-                var inGridAndSameChar = newI >= 0 && newI < height && newJ >= 0 && newJ < width && grid[newI][newJ] == currChar;
-                if (inGridAndSameChar) {
-                    if (grid[newI][newJ] != VISITED_MARKER) {
-                        grid[newI][newJ] = VISITED_MARKER;
-                        queue.offer(new Point(newI, newJ));
-                    }
-                } else {
-                    perimeterContribution++;
+                // Count grid boundaries and different characters as perimeter
+                if (newI < 0 || newI >= height || newJ < 0 || newJ >= width ||
+                        (grid[newI][newJ] != currChar && grid[newI][newJ] != visitedChar)) {  // Removed the VISITED_MARKER check
+                    // System.out.printf("i=%d j=%d offset=(%d, %d)\n", curr.i, curr.j, offset[0], offset[1]);
+                    perimeter++;
+                }
+                // Add unvisited neighbors of same character to queue
+                if (newI >= 0 && newI < height && newJ >= 0 && newJ < width &&
+                        grid[newI][newJ] == currChar && grid[newI][newJ] != visitedChar) {
+                    grid[newI][newJ] = visitedChar;
+                    queue.offer(new Point(newI, newJ));
                 }
             }
-            area++;
-            perimeter += perimeterContribution;
+            // System.out.printf("area=%d perim=%d\n", area, perimeter);
         }
 
         return new ExploredRegion(currChar, area, perimeter);
