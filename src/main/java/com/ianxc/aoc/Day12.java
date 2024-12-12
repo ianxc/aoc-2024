aa08c9d6ab45706f48de601149d520519ad5b459
Ian Chen <ianchen7777@gmail.com>
2024-12-12 19:34:15 +1100

    Day 12 part 1 attempt 1 wrong

---
+++ src/main/java/com/ianxc/aoc/Day12.java
@@ -0,0 +1,70 @@
+package com.ianxc.aoc;
+
+import java.util.ArrayDeque;
+
+public class Day12 {
+    private static final char VISITED_MARKER = '.';
+
+    private static final int[][] offsets = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+    static long part1(String path) {
+        final var grid = parseInput(path);
+        final var height = grid.length;
+        final var width = grid[0].length;
+        var totalPrice = 0L;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (grid[i][j] != VISITED_MARKER) {
+                    final var region = exploreRegion(grid, i, j);
+                    System.out.println(region);
+                    totalPrice += region.area * region.perimeter;
+                }
+            }
+        }
+        return totalPrice;
+    }
+
+    static ExploredRegion exploreRegion(char[][] grid, int i, int j) {
+        final var height = grid.length;
+        final var width = grid[0].length;
+        final var currChar = grid[i][j];
+        System.out.printf("Explore region i=%d j=%d char=%c\n", i, j, currChar);
+        var area = 0L;
+        var perimeter = 0L;
+        final var queue = new ArrayDeque<Point>();
+        queue.offer(new Point(i, j));
+        grid[i][j] = VISITED_MARKER;
+        while (!queue.isEmpty()) {
+            final var curr = queue.poll();
+            // record statistics and mark as visited
+            var perimeterContribution = 4L;
+            for (final var offset : offsets) {
+                final var newI = curr.i + offset[0];
+                final var newJ = curr.j + offset[1];
+                if (newI >= 0 && newI < height && newJ >= 0 && newJ < width) {
+                    if (grid[newI][newJ] == currChar) {
+                        perimeterContribution--;
+                        if (grid[newI][newJ] != VISITED_MARKER) {
+                            grid[newI][newJ] = VISITED_MARKER;
+                            queue.offer(new Point(newI, newJ));
+                        }
+                    }
+                }
+            }
+            area++;
+            perimeter += perimeterContribution;
+        }
+
+        return new ExploredRegion(currChar, area, perimeter);
+    }
+
+    static char[][] parseInput(String path) {
+        return Util.loadFile(path).stream().map(String::toCharArray).toArray(char[][]::new);
+    }
+
+    record ExploredRegion(char ch, long area, long perimeter) {
+    }
+
+    record Point(int i, int j) {
+    }
+}
---
+++ src/test/java/com/ianxc/aoc/Day12Test.java
@@ -0,0 +1,20 @@
+package com.ianxc.aoc;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day12Test {
+    @ParameterizedTest
+    @CsvSource({
+            "day12/demo.txt, 140",
+            "day12/demo2.txt, 772",
+            "day12/demo3.txt, 1930",
+    })
+    void testPart1(String path, long expectedSol) {
+        var res = Day12.part1(path);
+
+        assertEquals(expectedSol, res);
+    }
+}
