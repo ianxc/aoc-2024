4bb1a4d6194b44b0832f9fe86e1a0e16d2fe8687
Ian Chen <ianchen7777@gmail.com>
2024-12-13 18:01:54 +1100

    Day 13 part 2 attempt not working

--- src/main/java/com/ianxc/aoc/Day13.java
+++ src/main/java/com/ianxc/aoc/Day13.java
@@ -1,6 +1,8 @@
 package com.ianxc.aoc;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class Day13 {
@@ -12,7 +14,15 @@ public class Day13 {
 
     static long part1(String path) {
         var input = Util.loadFile(path);
+        var scenarios = getScenarios(input);
+        System.out.println(scenarios);
+        return scenarios.stream()
+                .mapToLong(s -> minCostToReach(s.prize.x, s.prize.y, s.a.x, s.a.y, s.a.c, s.b.x, s.b.y, s.b.c))
+                .filter(moves -> moves != UNREACHABLE)
+                .sum();
+    }
 
+    private static ArrayList<Scenario> getScenarios(List<String> input) {
         var scenarios = new ArrayList<Scenario>();
         System.out.println(input);
         for (int i = 0; i < input.size(); i += 4) {
@@ -21,17 +31,13 @@ public class Day13 {
             var p = parsePrize(input.get(i + 2));
             scenarios.add(new Scenario(p, a, b));
         }
-        System.out.println(scenarios);
-        return scenarios.stream()
-                .mapToLong(s -> minCostToReach(s.prize.x, s.prize.y, s.a.x, s.a.y, s.a.c, s.b.x, s.b.y, s.b.c))
-                .filter(moves -> moves != UNREACHABLE)
-                .sum();
+        return scenarios;
     }
 
     static Button parseButton(String line) {
         System.out.println("parseButton: " + line);
         var m = buttonPattern.matcher(line);
-        m.find();
+        if (!m.find()) throw new IllegalStateException("invalid regex");
         var label = m.group(1).charAt(0);
         var cost = label == 'A' ? 3 : 1;
         return new Button(label, Integer.parseInt(m.group(2)), Integer.parseInt(m.group(3)), cost);
@@ -40,7 +46,7 @@ public class Day13 {
     static Point parsePrize(String line) {
         System.out.println("parsePrize: " + line);
         var m = prizePattern.matcher(line);
-        m.find();
+        if (!m.find()) throw new IllegalStateException("invalid regex");
         return new Point(Integer.parseInt(m.group(1)), Integer.parseInt(m.group(2)));
     }
 
@@ -57,8 +63,57 @@ public class Day13 {
                 }
             }
         }
-        System.out.println(minCost);
+        // System.out.println(minCost);
         return minCost;
+    }
+
+    static long minCostToReach2(int pa, int pb, int x1, int y1, int c1, int x2, int y2, int c2) {
+        final var modX = Math.max(x1, x2);
+        final var modY = Math.max(y1, y2);
+        final var dp = new long[modX][modY];
+        for (var d: dp) {
+            Arrays.fill(d, UNREACHABLE);
+        }
+        dp[0][0] = 0;
+        var changed = true;
+        while (changed) {
+            changed = false;
+            for (int a = 0; a < modX; a++) {
+                for (int b = 0; b < modY; b++) {
+                    if (dp[a][b] == UNREACHABLE) continue;
+
+                    var abCost = dp[a][b];
+                    var newA = (a + x1) % modX;
+                    var newB = (b + y1) % modY;
+                    var newCost = abCost + c1;
+                    if (newCost < dp[newA][newB]) {
+                        dp[newA][newB] = newCost;
+                        changed = true;
+                    }
+
+                    newA = (a + x2) % modX;
+                    newB = (b + y2) % modY;
+                    newCost = abCost + c2;
+                    if (newCost < dp[newA][newB]) {
+                        dp[newA][newB] = newCost;
+                        changed = true;
+                    }
+                }
+            }
+        }
+
+        final var targetRemX = pa % modX;
+        final var targetRemY = pb % modY;
+        return dp[targetRemX][targetRemY];
+    }
+
+    static long part2(String path) {
+        var input = Util.loadFile(path);
+        var scenarios = getScenarios(input);
+        return scenarios.stream()
+                .mapToLong(s -> minCostToReach2(s.prize.x, s.prize.y, s.a.x, s.a.y, s.a.c, s.b.x, s.b.y, s.b.c))
+                .filter(moves -> moves != UNREACHABLE)
+                .sum();
     }
 
     record Point(int x, int y) {
