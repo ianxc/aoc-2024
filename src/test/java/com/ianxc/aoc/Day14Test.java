7c67a1acb0fe5f1c4ac7c4464920e29dfcc5b529
Ian Chen <ianchen7777@gmail.com>
2024-12-14 18:00:17 +1100

    Day 14 part 1 done

--- src/test/java/com/ianxc/aoc/Day14Test.java
+++ src/test/java/com/ianxc/aoc/Day14Test.java
@@ -6,10 +6,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class Day14Test {
     @Test
-    void testPart1() {
+    void testPart1Demo() {
         var res = Day14.part1("day14/demo.txt", 7, 11, 100);
 
         assertEquals(12L, res);
+    }
+
+    @Test
+    void testPart1() {
+        var res = Day14.part1("day14/input.txt", 103, 101, 100);
+
+        assertEquals(224438715L, res);
+    }
+
+    @Test
+    void testPart1Demo2() {
+        var res = Day14.part1("day14/demo2.txt", 7, 11, 5);
+
+        assertEquals(0L, res);
     }
 
 }
