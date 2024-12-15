f467890331cdb2af031a29cc94796393c78f05d5
Ian Chen <ianchen7777@gmail.com>
2024-12-15 16:33:13 +1100

    Day 15 parsing done

--- src/test/java/com/ianxc/aoc/Day15Test.java
+++ src/test/java/com/ianxc/aoc/Day15Test.java
@@ -1,7 +1,14 @@
 package com.ianxc.aoc;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Day15Test {
+    @Test
+    void testPart1Demo() {
+        var res = Day15.part1("day15/demo.txt");
 
+        assertEquals(-1, res);
+    }
 }
