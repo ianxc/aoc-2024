60f50a4270f3371f60c961105f5bea2646595baa
Ian Chen <ianchen7777@gmail.com>
2024-12-16 22:05:34 +1100

    Day 16 part 1: cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ version = "1.0-SNAPSHOT"
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
