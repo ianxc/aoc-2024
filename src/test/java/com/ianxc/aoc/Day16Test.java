597ca42ffdd7a698b84f90ef599faafb9dc0a96b
Ian Chen <ianchen7777@gmail.com>
2024-12-16 22:59:36 +1100

    Day 16 part 2 setup -> 3d

--- src/test/java/com/ianxc/aoc/Day16Test.java
+++ src/test/java/com/ianxc/aoc/Day16Test.java
@@ -25,4 +25,22 @@ class Day16Test {
         assertEquals(130536, res);
     }
 
+    @ParameterizedTest
+    @CsvSource({
+            "day16/demo.txt, 7036",
+            "day16/demo2.txt, 11048"
+    })
+    void testPart2Demo(String path, long expected) {
+        var res = Day16.part2(path);
+
+        assertEquals(expected, res);
+    }
+
+    @Test
+    void testPart2() {
+        var res = Day16.part2("day16/input.txt");
+
+        assertEquals(130536, res);
+    }
+
 }
