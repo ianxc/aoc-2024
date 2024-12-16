3db7abe1bba05e37b876882f5f3ab5ca64b2c53f
Ian Chen <ianchen7777@gmail.com>
2024-12-16 21:09:15 +1100

    Day 16 part 1: A* not working :/

---
+++ src/test/java/com/ianxc/aoc/Day16Test.java
@@ -0,0 +1,15 @@
+package com.ianxc.aoc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day16Test {
+    @Test
+    void testPart1Demo() {
+        var res = Day16.part1("day16/demo.txt");
+
+        assertEquals(100, res);
+    }
+
+}
