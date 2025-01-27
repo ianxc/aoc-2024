2d9fb3e6959f82cd90d144fcbc42720a5c54b28d
Ian Chen <ianchen7777@gmail.com>
2025-01-27 13:49:12 +1100

    fix decode script

--- crypt-unlock.sh
+++ crypt-unlock.sh
@@ -1,4 +1,4 @@
 set -e;
 sudo apt update -q;
 sudo apt install -y git-crypt;
-echo ${{ secrets.GIT_CRYPT }} | base64 -- decode | git-crypt unlock /dev/stdin;
+echo ${{ secrets.GIT_CRYPT }} | base64 --decode | git-crypt unlock /dev/stdin;
